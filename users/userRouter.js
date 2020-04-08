9c7d2d1d5cfa49f23fbb7d6263fcc641e925c62e
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-08 17:10:19 -0500

    Custom Middleware Functions

--- users/userRouter.js
+++ users/userRouter.js
@@ -5,7 +5,7 @@ const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // do your magic!
   Users.insert(req.body)
     .then((user) => {
@@ -17,11 +17,11 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/:id/posts", (req, res) => {
+router.post("/:id/posts", validatePost, (req, res) => {
   // do your magic!
   Posts.insert(req.body)
     .then((post) => {
-      res.status(201).post;
+      res.status(201).json(post);
     })
     .catch((err) => {
       console.log(err);
@@ -41,7 +41,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
@@ -49,26 +49,68 @@ router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Error with database" });
+    });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res) => {
   // do your magic!
+  Users.update(req.params.id, req.body)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error with database" });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  if (req.params.id) {
+    Users.getById(req.params.id)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Error with database" });
+      });
+  } else {
+    res.status(400).json({ message: "invalid user id" });
+  }
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
